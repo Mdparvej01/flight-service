2f5ff47665c6d8dd8f9d142160b0f75a920924f1
Parvej <officialparvej.mo@gmail.com>
2024-10-13 20:58:57 +0530

    added base structure

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
- app.use('/api' , apiRoutes);
+ app.use('/api' , apiRoutes);  //.....
  
 app.listen(ServerConfig.PORT , ()=> {
     console.log(`Sucessfully started the server on PORT:${ServerConfig.PORT}`);
     // Logger.info("Sucessfully started the server" , "root" , {}) // bcz root index .js simply use Logger .error  
-});
+});     
